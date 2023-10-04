1ae92f206c0dbda7a6160e64b6ec45c34bebec2c
Aleksandr Sokolov <algensok@gmail.com>
2023-10-04 21:04:59 +0300

    Task 2. Add metod Reviewer and Student

--- student_and_mentor.py
+++ student_and_mentor.py
@@ -10,6 +10,15 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def rate_lectures(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
 
@@ -17,6 +26,16 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -28,24 +47,23 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-
-    pass
-
-
-class Reviewer(Mentor):
-
-    pass
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+super_lecturer = Lecturer('Bill', 'Smith')
+super_lecturer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+best_student.rate_lectures(super_lecturer, 'Python', 10)
+best_student.rate_lectures(super_lecturer, 'Python', 10)
+best_student.rate_lectures(super_lecturer, 'Python', 7)
+
+print(super_lecturer.grades)
